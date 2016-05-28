0f20e31edc65f2317b71b4cccbb8f01b3083a883
Xierip <xierip@gmail.com>
2016-05-29 00:34:51 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,4 +12,4 @@ if [ ! -f /data/spigot.jar ]; then
 fi
 
 cd /data
-java -XX:PermSize=128m -XX:MaxPermSize=256m -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSParallelRemarkEnabled -XX:ParallelGCThreads=3 -XX:MaxGCPauseMillis=5 -XX:+UseAdaptiveGCBoundary -XX:-UseGCOverheadLimit -XX:+UseBiasedLocking -XX:SurvivorRatio=8 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=15 -Xnoclassgc -XX:UseSSE=3 -XX:+UseFastAccessorMethods -XX:+UseStringCache -XX:+OptimizeStringConcat -XX:+AggressiveOpts  -Xms24M -Xmx3G -jar spigot.jar
+java -Xms24M -Xmx3G -jar spigot.jar
